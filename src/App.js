86a7e15c3cc516f4f7933f5852812303cd7568c9
ehcodes <engrid.hernandez@gmail.com>
2023-07-09 20:52:07 -0400

    Removed whitespace from styles.css, updated Square components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React, { StrictMode } from "react";
+// had to stray from the tutorial instructions, added a div instead of fragments to get around a compiling error
+// used a class name of boardRow instead of the original board-row
+function Square ({ value }){
+  
+  function handleClick(){
+    console.log(`Clicked! ${value}`);
+  }
 
-export default function Square() {
-  return <button className="square">X</button>;
+  return <button className="square" onClick={handleClick}>{value}</button> 
+}
+export default function Board() {
+  return (
+    <div>
+      <div className="boardRow">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="boardRow">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="boardRow">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
+    </div>
+  );
 }
